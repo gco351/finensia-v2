ecadf2cd6bc08dcd7aa5a4f00356f7afae56baa1
gco351 <gco351@gmail.com>
2026-04-10 18:16:09 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          500: '#f97316',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
